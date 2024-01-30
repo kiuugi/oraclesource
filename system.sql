6e74a6f7b0bb5217f048fd5276ed96086ac98ec8
kiuugi <jeung9963@gmail.com>
2024-01-30 17:55:35 +0900

    제약조건

--- system.sql
+++ system.sql
@@ -18,3 +18,64 @@ ALTER USER SCOTT TEMPORARY TABLESPACE TEMP;
 ALTER USER SCOTT QUOTA 10M ON USERS;
 
 alter session set "_oracle_script"=true;
+
+-- DBA 권한을 가진 사용자만 사용자를 생성하고 권한을 부여 가능.
+-- SYSTEM, sys as sysdba 
+-- CREATE USER 아이디 IDENTIFIED BY 비밀번호 ;
+-- 공통 사용자 또는 롤 이름이 부적합합니다. // C## 를 아이디앞에붙임 오라클 21c xe버전에서 있는 요청사항
+CREATE USER C##test IDENTIFIED BY test ;
+
+-- CREATE SESSION 권한을 가지고 있지 않음; 로그온 거절
+--권한부여 
+-- SESSION, CREATE(USER, TABLE, SEQUENCE, VIEW, ...)
+-- 권한 그룹 => 롤
+-- CONNECT, RESOURCE
+-- CONNECT(SESSION, CREATE TABLE, CREATE VIEW, ...)
+-- RESOURCE(SEQUENCE, 프로시저, 테이블, ...)
+
+-- GRANT 권한이름, ... TO 사용자아이디
+-- GRANT 롤이름, ... TO 사용자아이디
+GRANT CONNECT, resource TO C##test;
+
+CREATE USER C##test2 IDENTIFIED BY test
+DEFAULT TABLESPACE users
+TEMPORARY TABLESPACE TEMP
+QUOTA 10M ON USERS;
+
+GRANT CONNECT, resource TO C##test2;
+-- 유저 생성, 사용할 공간, 공간 크기, 권한 부여
+
+-- REVOKE 취소할 권한 이름, ... FROM 권한을 뺏을 유져 아이디
+
+-- 비밀번호 변경
+-- ALTER USER 유저아이디 IDENTIFIED BY 비밀번호;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
